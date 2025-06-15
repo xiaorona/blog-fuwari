3fe0f9c2c8a034645be7a2c4553a456360ed0c69
xiaorona <ekoneko@126.com>
2025-06-15 20:15:06 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,85 +1,90 @@
 import type {
-	ExpressiveCodeConfig,
-	LicenseConfig,
-	NavBarConfig,
-	ProfileConfig,
-	SiteConfig,
+        ExpressiveCodeConfig,
+        LicenseConfig,
+        NavBarConfig,
+        ProfileConfig,
+        SiteConfig,
 } from "./types/config";
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Sugar",
-	subtitle: "blog",
-	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
-	themeColor: {
-		hue: 345, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
-	},
-	banner: {
-		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
-		credit: {
-			enable: true, // Display the credit text of the banner image
-			text: "ARONA,by 咔咔", // Credit text to be displayed
-			url: "https://www.pixiv.net/artworks/116135803", // (Optional) URL link to the original artwork or artist's page
-		},
-	},
-	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
-	},
-	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
-	],
+        title: "Sugar",
+        subtitle: "blog",
+        lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+        themeColor: {
+                hue: 345, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+                fixed: false, // Hide the theme color picker for visitors
+        },
+        banner: {
+                enable: true,
+                src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+                position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+                credit: {
+                        enable: true, // Display the credit text of the banner image
+                        text: "ARONA,by 咔咔", // Credit text to be displayed
+                        url: "https://www.pixiv.net/artworks/116135803", // (Optional) URL link to the original artwork or artist's page
+                },
+        },
+        toc: {
+                enable: true, // Display the table of contents on the right side of the post
+                depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+        },
+        favicon: [
+                // Leave this array empty to use the default favicon
+                // {
+                //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+                //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+                //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+                // }
+        ],
 };
 
 export const navBarConfig: NavBarConfig = {
-	links: [
-		LinkPreset.Home,
-		LinkPreset.Archive,
-		LinkPreset.About,
-		{
+        links: [
+                LinkPreset.Home,
+                LinkPreset.Archive,
+                LinkPreset.About,
+                {
                 name: "友链",
                 url: "https://blog.sugar.moe/friend-link.html",
                 external: true,
                 },
-	       ],
+                {
+                name: "Sugar's home",
+                url: "https://www.sugar.moe/",
+                external: true,
+                },
+               ],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "星沉",
-	bio: "回首向来萧瑟处，也无风雨也无晴。",
-	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://x.com/xiaoplana",
-		},
-		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/xiaorona",
-		},
-	],
+        avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+        name: "星沉",
+        bio: "回首向来萧瑟处，也无风雨也无晴。",
+        links: [
+                {
+                        name: "Twitter",
+                        icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+                        // You will need to install the corresponding icon set if it's not already included
+                        // `pnpm add @iconify-json/<icon-set-name>`
+                        url: "https://x.com/xiaoplana",
+                },
+                {
+                        name: "GitHub",
+                        icon: "fa6-brands:github",
+                        url: "https://github.com/xiaorona",
+                },
+        ],
 };
 
 export const licenseConfig: LicenseConfig = {
-	enable: true,
-	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+        enable: true,
+        name: "CC BY-NC-SA 4.0",
+        url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
-	theme: "github-dark",
+        // Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+        // Please select a dark theme, as this blog theme currently only supports dark background color
+        theme: "github-dark",
 };
