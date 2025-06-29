9ad17478158337c3a86e7599a28099f3f38d4609
xiaorona <ekoneko@126.com>
2025-06-29 08:05:32 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
         title: "Sugar's blog",
-        subtitle: "🍬",
+        subtitle: "welcome!",
         lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
         themeColor: {
                 hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
